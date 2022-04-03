4194c3cc2b9e9090c767144503754ab55bc8b7de
Allison Adad <allisonadad@Allisons-MBP.lan>
2022-04-02 17:43:39 -0700

    nav bar on home.jsx

--- application/my-app/frontend/src/pages/home.jsx
+++ application/my-app/frontend/src/pages/home.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import NavBar from '../components/Navbar';
+import Search from '../components/Search';
 
 const Home = () => {
 
