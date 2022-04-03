da929f4bae7c81a3fb7c50af29c6a540441183b9
Allison Adad <allisonadad@Allisons-MBP.lan>
2022-04-02 17:46:28 -0700

    home.jsx updated

--- application/my-app/frontend/src/pages/home.jsx
+++ application/my-app/frontend/src/pages/home.jsx
@@ -6,35 +6,35 @@ import Search from '../components/Search';
 
 const Home = () => {
 
-    const [title, setTitle] = useState("");
-    const [field, setField] = useState("");
-    const [resData, setResData] = useState([]);
-    const ip = "13.52.76.208";
+    // const [title, setTitle] = useState("");
+    // const [field, setField] = useState("");
+    // const [resData, setResData] = useState([]);
+    // const ip = "13.52.76.208";
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        axios.post("http://" + ip + ":3001/search", { title, field })
-            .then(res => {
-                console.log(res.data.result);
-                setResData(res.data.result);
-            });
-    };
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     axios.post("http://" + ip + ":3001/search", { title, field })
+    //         .then(res => {
+    //             console.log(res.data.result);
+    //             setResData(res.data.result);
+    //         });
+    // };
 
-    useEffect(() => {
-    }, []);
+    // useEffect(() => {
+    // }, []);
 
     return (
         <div>
-            <title>
+            {/* <title>
                 Gitjob Website
             </title>
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="stylesheet" href="css\home.css" />
+            <link rel="stylesheet" href="css\home.css" /> */}
             
             <NavBar/>
 
-            {resData.map(post => (
+            {/* {resData.map(post => (
             <div>
                 <div>Job Name: {post["job name"]}</div>
                 <div>Job Field: {post["job field"]}</div>
@@ -43,7 +43,7 @@ const Home = () => {
                 <div>Job Salary: {post["job salary"]}</div>
                 <br/>
             </div>
-            ))}
+            ))} */}
         </div>
     );
 };
