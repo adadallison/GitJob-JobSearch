a3f355f10bb713c6842b46f872f5c89d8b076573
Allison Adad <allisonadad@Allisons-MBP.lan>
2022-04-03 19:02:45 -0700

    login.js update

--- application/my-app/frontend/src/pages/Login.js
+++ application/my-app/frontend/src/pages/Login.js
@@ -1,31 +1,39 @@
 import React, {useState} from 'react';
 import "../css/login.css";
-import Search from '../components/Search';
 
 function Login(){
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("")
 
+    const handleSubmit = event => {
+        event.preventDefault();
+        alert('You have submitted the form.')
+      }
+
 
     return (
         <div>
             <div id="login" >GitJob</div>
-            <form id ="form">
-                
-                <label name="username">Username: </label>
-                <input type="text" required name="username" ></input>
-                <label name="password">Password: </label>
-                <input type="password" required name="password" ></input>
-                <input type="submit" style={{ gridColumn: "2", width: 100, justifySelf: "right" }} value="Login" />
+            <form onSubmit={handleSubmit}>
+                <div id ="form">
+                    <div><label name="username">Username</label> </div>
+                    <input type="text" required name="username" ></input>
+                </div>
+                <div id="labels">
+                    <div><label name="password">Password</label> </div>
+                    <input type="password" required name="password" ></input>
+                </div>
+    
+                <button type="submit">Submit</button>
             </form>
-
         </div>
-
-
 
     );
 }
 
 
 export default Login
+
+
+// https://www.digitalocean.com/community/tutorials/how-to-build-forms-in-react
