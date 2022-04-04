c61da9ea1a933fb4508c7a45235facdeab27274b
Allison Adad <allisonadad@Allisons-MBP.lan>
2022-04-04 13:12:15 -0700

    css class

--- application/my-app/frontend/src/pages/Register.js
+++ application/my-app/frontend/src/pages/Register.js
@@ -13,19 +13,19 @@ function Register(){
             <form>
                 <div id ="form">
                     <div><label name="name">Name  </label></div>
-                    <input type="text" required name="name" ></input>
+                    <input input className="input" type="text" required name="name" ></input>
                 </div>
                 <div id ="form">
                     <div><label name="email">Email  </label></div>
-                    <input type="text" required name="email" ></input>
+                    <input input className="input" type="text" required name="email" ></input>
                 </div>
                 <div id ="form">
                     <div><label name="password">Password</label></div>
-                    <input type="password" required name="password" ></input>
+                    <input input className="input" type="password" required name="password" ></input>
                 </div>
                 <div id ="form">
                     <div><label name="password">Confirm Password</label></div>
-                    <input type="password" required name="password" ></input>
+                    <input input className="input" type="password" required name="password" ></input>
                 </div>
                 <div id ="form">
                     <input type="radio" value="Job-Hunter"></input>Job-Hunter
