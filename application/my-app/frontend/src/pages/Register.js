39a332fd747fae921d0486d92a75d832eca5c2ac
Allison Adad <allisonadad@Allisons-MBP.lan>
2022-04-04 11:39:23 -0700

    register css updates

--- application/my-app/frontend/src/pages/Register.js
+++ application/my-app/frontend/src/pages/Register.js
@@ -1,40 +1,39 @@
 import React, {useState} from 'react';
+import "../css/login.css";
 
 function Register(){
 
     const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("")
-
-
+    const [password, setPassword] = useState("");
 
 
     return (
         <div>
             <div id="login" >GitJob</div>
-            <form id ="form">
-                <div>
-                    <label name="name">Name  </label>
+            <form>
+                <div id ="form">
+                    <div><label name="name">Name  </label></div>
                     <input type="text" required name="name" ></input>
                 </div>
-                <div>
-                    <label name="email">Email  </label>
+                <div id ="form">
+                    <div><label name="email">Email  </label></div>
                     <input type="text" required name="email" ></input>
                 </div>
-                <div>
-                    <label name="password">Password  </label>
+                <div id ="form">
+                    <div><label name="password">Password</label></div>
                     <input type="password" required name="password" ></input>
                 </div>
-                <div>
-                    <label name="password">Confirm Password  </label>
+                <div id ="form">
+                    <div><label name="password">Confirm Password</label></div>
                     <input type="password" required name="password" ></input>
                 </div>
-                <div>
-                    <input type="radio" value="Job-Hunter   "></input>
-                    <input type="radio" value="Recruiter    "></input>
-                    <input type="radio" value="Company    "></input>
+                <div id ="form">
+                    <input type="radio" value="Job-Hunter"></input>Job-Hunter
+                    <input type="radio" value="Recruiter"></input>Recruiter
+                    <input type="radio" value="Company"></input>Company
                 </div>
                 <div>
-                    <input type="submit" style={{ gridColumn: "2", width: 100, justifySelf: "right" }} value="Register"/>
+                    <button type="submit">Register</button>
                 </div>
             </form>
 
