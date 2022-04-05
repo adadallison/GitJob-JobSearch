757b50dbc3145c8c17513cdfca16a5d146445f3f
Allison Adad <allisonadad@Allisons-MBP.lan>
2022-04-04 19:33:22 -0700

    added career page and edited side nav

--- application/my-app/frontend/src/pages/Careers.js
+++ application/my-app/frontend/src/pages/Careers.js
@@ -5,7 +5,7 @@ import NavBar from '../components/Navbar';
 import { Link } from 'react-router-dom';
 import {IconContext} from 'react-icons';
 import {SidebarData} from '../components/SidebarData';
-import "../css/home.css";
+import "../css/careers.css";
 import "../css/Navbar.css";
 
 const Careers = () => {
@@ -33,37 +33,18 @@ const Careers = () => {
                     </div>  
                 </div>
 
-                <div class="item-center" >TWO
-                    <div class="item-center1">Example Post 1</div>
-                    <div class="item-center1">Example Post 2</div>
-                    <div class="item-center1">Example Post 3</div>
-                    <div class="item-center1">Example Post 4</div>
-                    <div class="item-center1">Example Post 5</div>
-                    <div class="item-center1">Example Post 6</div>             
-                    <div class="item-center1">Example Post 7</div>
-                    <div class="item-center1">Example Post</div>
-                    <div class="item-center1">Example Post</div>
-                    <div class="item-center1">Example Post</div>
-                    <div class="item-center1">Example Post</div>
-                    <div class="item-center1">Example Post</div>
-                    <div class="item-center1">Example Post</div>
-                    <div class="item-center1">Example Post</div>
-                    <div class="item-center1">Example Post</div>
-                    <div class="item-center1">Example Post</div>
-                    <div class="item-center1">1</div>
-                    <div class="item-center1">3</div>
-                    <div class="item-center1">1</div>
-                    <div class="item-center1">2</div>
-                    <div class="item-center1">3</div>
-                    <div class="item-center1">1</div>
-                    <div class="item-center1">2</div>
-                    <div class="item-center1">3</div>
-                    <div class="item-center1">1</div>
+                <div class="career-fields-container" >
+                    <div class="fields">AI and ML</div>
+                    <div class="fields">Robotics Process Automation</div>
+                    <div class="fields">Internet of Things</div>
+                    <div class="fields">5G</div>
+                    <div class="fields">Virtual and Augmented Reality</div>
+                    <div class="fields">Blockchain</div>
+                    <div class="fields">Quantum Computing</div>
+                    <div class="fields">Edge Computing</div>
+                    <div class="fields">Cyber Security</div>
                     
 
-                </div>
-
-                <div class="item-3">THREE
                 </div>
 
             </div>
