4cd5fa45d14cae94f0043963cbeded3f14472a94
Allison Adad <allisonadad@Allisons-MBP.lan>
2022-04-05 14:32:20 -0700

    apply button for job posts

--- application/my-app/frontend/src/pages/JobPosting.js
+++ application/my-app/frontend/src/pages/JobPosting.js
@@ -6,6 +6,7 @@ import { IconContext } from 'react-icons';
 import { SidebarData } from '../components/SidebarData';
 import "../css/careers.css";
 import NavBar from '../components/Navbar';
+import Register from './Register';
 
 const JobPosting = () => {
 
@@ -33,9 +34,7 @@ const JobPosting = () => {
 
                 <div class="center-info" >
                     <h1>Job Position @ Company</h1>
-                    <button class="apply-button" href="./Register">
-                        Apply
-                    </button>
+                    <button><a class="apply-button" href="./Register">Button</a></button>
                     <div class="about">
                         This is the job description. It will be decently long and contain all the ins and outs of
                         the position to be applied for. It will also explain the qualifications to the applicant.
