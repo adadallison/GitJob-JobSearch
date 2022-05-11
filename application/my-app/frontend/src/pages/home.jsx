a7263ea50587e84fda99a4e7434a4ccf209b9b4e
Allison Adad <allisonadad@Allisons-MBP.lan>
2022-05-10 17:06:17 -0700

    scroll container issue fixed

--- application/my-app/frontend/src/pages/home.jsx
+++ application/my-app/frontend/src/pages/home.jsx
@@ -222,7 +222,7 @@ const Home = () => {
                     <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
                         <div>
                             <h4>{tempPost["job name"]} Description</h4> 
-                            <div>{tempPost["job location"]}</div> <br></br>
+                            <div className='jobLocation' >{tempPost["job location"]}</div> <br></br>
 
                             <p className='descriptionScroll'>
                                 {tempPost["job desc."]}
